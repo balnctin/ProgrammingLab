78570070ec52e2187f9e7c48e02dc68ef578473c
Tetyana Franchuk <94189524+balnctin@users.noreply.github.com>
2021-11-18 08:57:41 +0000

    Sum of the elements in the list using range

--- sumlist.py
+++ sumlist.py
@@ -1,6 +1,16 @@
 #input 1,2,3,4,5
 #output 15
 #using sum()
+#the_list = [1,2,3,4,5]
+#total = sum(the_list)
+#print ('Sum  of elements in the list:', total)
+
+#sum using range
+
 the_list = [1,2,3,4,5]
-total = sum(the_list)
-print ('Sum in the list:', total)
+sum = 0
+
+for i in range (0, len(the_list)):
+  sum = sum + the_list[i]
+
+print (" Sum of elements in the list:", sum)
