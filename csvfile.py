2f6547add0ce35782bb0e49a43c7c8f9dbf1066f
Tetyana Franchuk <94189524+balnctin@users.noreply.github.com>
2021-11-26 13:08:56 +0000

    Ho aggiunto print('Name: {}'.format(self.name))

--- csvfile.py
+++ csvfile.py
@@ -1,6 +1,7 @@
 class CSVFile():
   def __init__ (self, name):
       self.name = name
+      print('Name: {}'.format(self.name))
   def get_data(self):
       data = []
       my_file = open(self.name, 'r')
@@ -8,7 +9,7 @@ class CSVFile():
           elements = line.split (',')
       if elements[0]!='Date':
           data.append(elements)
-          print('Dati nel file: {}'.format(self.name))
+          print('Dati nel file: {}'.format(get.data))
       my_file.close()
       return data
 CSVFile("shampoo_sales.csv")
