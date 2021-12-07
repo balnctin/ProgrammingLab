5aa4f02aa4b6adf3135d35644a12449a1c3bff3d
Tetyana Franchuk <94189524+balnctin@users.noreply.github.com>
2021-12-07 17:03:22 +0000

    Modello dell'esercizio (non fatto)

---
+++ models.py
@@ -0,0 +1,17 @@
+class Model():
+  def fit(self, data):
+      #Fit non implementanto nella classe base
+      raise NotImplementedError('Metodo non implementato')
+
+  def predict (self, data):
+      #Predict non implementanto nella classe base
+      raise NotImplementedError('Metodo non implementato')
+
+class IncrementModel(Model):
+  def predict(self,data):
+      prev_values = []
+      for item in data:
+          #Logica per la predizione
+          ...
+          prediction = ...
+          return prediction
